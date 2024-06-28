4e65dd94427d3aeab1433c158aa6f4cbf7b3785c
add <992361951@qq.com>
2024-06-28 23:42:16 +0800

    first push

---
+++ helloNumbers.java
@@ -0,0 +1,12 @@
+public class helloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int sum = 0;
+        while (x < 10) {
+            sum += x;
+            x += 1;
+            System.out.println(sum);
+
+        }
+    }
+}
